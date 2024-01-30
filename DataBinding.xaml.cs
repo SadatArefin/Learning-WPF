8a611af86c1cdf37c9b806dcd560e67c77e0e28e
Sadat Arefin Rafat <sadat.arefin.rafat@gmail.com>
2024-01-30 12:29:12 +0600

    Simple Data Binding Example

---
+++ DataBinding.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfSampleApplication
+{
+    /// <summary>
+    /// Interaction logic for DataBinding.xaml
+    /// </summary>
+    public partial class DataBinding : Window
+    {
+        public DataBinding()
+        {
+            InitializeComponent();
+
+            Person person = new();
+
+            DataContext = person;
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; } = "No Name";
+    }
+}

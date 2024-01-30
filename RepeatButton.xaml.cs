e7c667628115ef50b30e188bde120eb621a0bfd1
Sadat Arefin Rafat <sadat.arefin.rafat@gmail.com>
2024-01-30 11:03:03 +0600

    Repeat Button

---
+++ RepeatButton.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfSampleApplication
+{
+    /// <summary>
+    /// Interaction logic for RepeatButton.xaml
+    /// </summary>
+    public partial class RepeatButton : Window
+    {
+        public RepeatButton()
+        {
+            InitializeComponent();
+        }
+
+        void Increase(object sender, RoutedEventArgs e)
+        {
+            Int32 Num = Convert.ToInt32(valueText.Text);
+
+            valueText.Text = ((Num + 1).ToString());
+        }
+
+        void Decrease(object sender, RoutedEventArgs e)
+        {
+            Int32 Num = Convert.ToInt32(valueText.Text);
+
+            valueText.Text = ((Num - 1).ToString());
+        }
+    }
+}
